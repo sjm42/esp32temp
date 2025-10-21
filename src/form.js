1d74999e38e324634267e9323ec1bda7540b4019
Sami J. Mäkinen <sjm@iki.fi>
2025-10-21 16:02:51 +0300

    Tweaks

--- src/form.js
+++ src/form.js
@@ -1,7 +1,51 @@
 // form.js for esp32temp
-var postCfgDataAsJson = async ({
-                                   url, formData
-                               }) => {
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("form[name='esp32cfg']")
+        .addEventListener("submit", handleCfgSubmit);
+    setInterval(update_uptime, 10e3);
+    setInterval(update_temperatures, 60e3);
+});
+
+async function update_uptime() {
+    let o = document.getElementById("uptime");
+    // o.innerHTML = "<p>Uptime: -updating-</p>";
+    const url = "/uptime";
+    const response = await fetch(url);
+    const json = JSON.parse(await response.text());
+    o.innerHTML = `<p>Uptime: ${json.uptime} (${json.uptime_s})</p>`;
+}
+
+async function update_temperatures() {
+    let o = document.getElementById("temperatures");
+    // o.innerHTML = "<p>-updating-</p>";
+    const url = "/temp";
+    const response = await fetch(url);
+    const json = JSON.parse(await response.text());
+    let rows = "<tr><th>iopin</th><th>sensor</th><th>value</th></tr>\n";
+    json.temperatures.forEach((temp, index) => {
+        rows += `<tr><td><i>${temp.iopin}</i></td><td>${temp.sensor}</td><td><b>${temp.value}</b></td></tr>\n`;
+    });
+    o.innerHTML = `<p>Last update: <b>${json.last_update}</b><br>\n<table>\n${rows}</table></p>\n`;
+}
+
+let handleCfgSubmit = async (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const url = form.action;
+
+    try {
+        const formData = new FormData(form);
+        const responseData = await postCfgDataAsJson({url, formData});
+        console.log({
+            responseData
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+let postCfgDataAsJson = async ({url, formData}) => {
     const formObj = Object.fromEntries(formData.entries());
     // convert integers
     formObj.port = parseInt(formObj.port);
@@ -29,42 +73,4 @@ var postCfgDataAsJson = async ({
 
     return response.json();
 }
-
-var handleCfgSubmit = async (event) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const url = form.action;
-
-    try {
-        formData = new FormData(form);
-        const responseData = await postCfgDataAsJson({
-            url, formData
-        });
-        console.log({
-            responseData
-        });
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector("form[name='esp32cfg']")
-        .addEventListener("submit", handleCfgSubmit);
-});
-
-async function update_uptime() {
-    var o = document.getElementById("uptime");
-    o.innerHTML = "Updating...";
-    var url = "/uptime";
-    const response = await fetch(url);
-    const json = JSON.parse(await response.text());
-    o.innerHTML = `<p>Uptime: ${json.uptime} (${json.uptime_s}) </p>`;
-}
-
-function onLoad() {
-    // update_uptime();
-    setInterval(update_uptime, 10e3);
-}
-
 // EOF
